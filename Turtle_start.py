6086ea319877b7b7317d7d4ad1a6545325c9b878
ericdurigan <89487642+ericdurigan@users.noreply.github.com>
2021-09-08 19:16:40 -0600

    Jeff Bezos illuminati

--- Turtle_start.py
+++ Turtle_start.py
@@ -26,10 +26,50 @@ panel.bgcolor("lightsteelblue1")
 panel.bgpic(image)
 
 #=======Add your code here======
+turtle.Turtle()
+turtle.penup()
+turtle.goto(22,70)
+turtle.pendown()
+turtle.pensize(5)
+turtle.forward(50)
+turtle.circle(20)
+turtle.backward(100)
+turtle.circle(20)
+turtle.penup()
+turtle.goto(10,15)
+turtle.pendown()
+turtle.forward(25)
+turtle.right(120)
+turtle.forward(25)
+turtle.right(120)
+turtle.forward(25)
+turtle.penup()
+
+turtle.goto(180,50)
+turtle.pendown()
+turtle.pensize(15)
+turtle.color(20,50,100)
+turtle.begin_fill()
+turtle.fillcolor("red")
+turtle.forward(50)
+turtle.right(72)
+turtle.forward(50)
+turtle.right(72)
+turtle.forward(50)
+turtle.right(72)
+turtle.forward(50)
+turtle.right(72)
+turtle.forward(50)
+turtle. end_fill()
+
+
+
+
 
 
 
 
 #=======Clean up code (do not change)======
 # this code ensures that your script runs correctly each time.
-turtle.done()
+#   turtle.done()
+''
